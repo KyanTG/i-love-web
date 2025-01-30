adb68f5fd122c1de60e0eccd311a7b9f79622529
KyanTG <kyan.gijtenbeek@hva.nl>
2025-01-30 14:33:30 +0100

    made one animated article and fixed the hamburger menu

--- Code/main.js
+++ Code/main.js
@@ -1,6 +1,5 @@
 
-
-
+// Hamburger menu
 
 const openButton = document.querySelector(".hamburger");
 
@@ -17,3 +16,14 @@ function openMenu() {
 
 const sluitButton = document.querySelector(".hamburger-open");
 
+
+// Voortgangs pagina divjes bij click info laten zien
+
+const ShowMore = document.querySelector(".show-more");
+
+ShowMore.addEventListener("click", ShowText);
+
+function ShowText() {
+  const InfoDiv = document.querySelector(".show-more-article");
+  InfoDiv.classList.toggle("div-show-p");
+}
