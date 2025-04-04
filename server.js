139caa26a1691615d5bbeb9ce3c1054220b23e89
KyanTG <kyan.gijtenbeek@hva.nl>
2025-04-04 15:02:05 +0200

    installed liquid & made partials / views and made liquid pages with dynamic headers / head

--- server.js
+++ server.js
@@ -7,7 +7,41 @@ app.use(express.static('public'))
 
 app.use(express.urlencoded({extended: true}))
 
+
+import { Liquid } from 'liquidjs';
+
+
+const engine = new Liquid();
+app.engine('liquid', engine.express()); 
+
+
+app.set('views', './views')
+
+
+
+
+
+app.get('/', async function (request, response) {
+    response.render('home.liquid')
+})
+
+app.get('/portfolio', async function (request, response) {
+    response.render('portfolio.liquid')
+})
+
+app.get('/extras', async function (request, response) {
+    response.render('extras.liquid')
+})
+
+app.get('/voortgang', async function (request, response) {
+    response.render('voortgang.liquid')
+})
+
+
+
+
 app.set('port', process.env.PORT || 8000)
+
 
 app.listen(app.get('port'), function () {
 
