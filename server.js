40b9de6e5fc226d1074a69609151f5d2a7e23ca2
KyanTG <kyan.gijtenbeek@hva.nl>
2025-10-25 14:50:55 +0200

    added subcontent pages for portfolio

--- server.js
+++ server.js
@@ -43,33 +43,48 @@ app.get('/portfolio', async function (request, response) {
     response.render('portfolio.liquid')
 });
 
-app.get('/extras', async function (request, response) {
-    response.render('extras.liquid')
+app.get('/portfolio/informaat', async function (request, response) {
+    response.render('informaat.liquid')
 });
 
-app.get('/voortgang', async function (request, response) {
-    response.render('voortgang.liquid', {files: files})
+app.get('/portfolio/mediahuis', async function (request, response) {
+    response.render('mediahuis.liquid')
 });
 
-app.get('/new', async function(request, response) {
-    response.render('new.liquid')
+app.get('/portfolio/embassy-of-the-free-mind', async function (request, response) {
+    response.render('embassy.liquid')
 });
 
-app.get('/voortgang/:slug', async function (request, response) {
-
-    console.log(request.params.slug)
-
-
-    const fileContents = await readFile('markdowns/' + request.params.slug + '.md', { encoding: 'utf8' })    
-    const markedContent = marked.parse(fileContents)
-
-
-    response.render('artikel.liquid', {fileContents: markedContent})
+app.get('/ervaring', async function (request, response) { 
+    response.render('ervaring.liquid')
 });
 
-app.get('/contact', async function (request, response) { 
-    response.render('contact.liquid')
-});
+
+
+// app.get('/extras', async function (request, response) {
+//     response.render('extras.liquid')
+// });
+
+// app.get('/voortgang', async function (request, response) {
+//     response.render('voortgang.liquid', {files: files})
+// });
+
+// app.get('/new', async function(request, response) {
+//     response.render('new.liquid')
+// });
+
+// app.get('/voortgang/:slug', async function (request, response) {
+
+//     console.log(request.params.slug)
+
+
+//     const fileContents = await readFile('markdowns/' + request.params.slug + '.md', { encoding: 'utf8' })    
+//     const markedContent = marked.parse(fileContents)
+
+
+//     response.render('artikel.liquid', {fileContents: markedContent})
+// });
+
 
 
 
